19e1b21e10e37c41e548adb9353e862e8c653bcd
GinaFerl <ginaferlindawati@gmail.com>
2021-04-20 09:57:33 +0700

    first commit

---
+++ app/src/main/java/com/gferl/ginaukk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gferl.ginaukk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
