e198eda214c93765d759a967f37c1508c39b348c
GinaFerl <ginaferlindawati@gmail.com>
2021-04-20 11:40:20 +0700

    fourth commit

--- app/src/main/java/com/gferl/todoapplication/MainActivity.java
+++ app/src/main/java/com/gferl/todoapplication/MainActivity.java
@@ -1,14 +1,53 @@
 package com.gferl.todoapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set default Home Fragment
+        loadFragment (new HomeFragment());
+
+        // deklarasi BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+
+        // listener saat item bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.add_menu:
+                fragment = new AddFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
